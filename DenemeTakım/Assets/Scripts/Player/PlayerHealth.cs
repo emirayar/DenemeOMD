35119884db1a541e6e1057ca3e5703b1b8fb27a4
EmirhanSen58 <senemirhan5834@gmail.com>
2024-02-16 20:28:25 +0300

    healtbar animation eklendi

--- DenemeTakım/Assets/Scripts/Player/PlayerHealth.cs
+++ DenemeTakım/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,25 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UI;       
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    [SerializeField] private int currentHealth;
-    public Slider slider; // 15.02.2024 slider deðiþkeni eklendi
+    public float maxHealth = 100f;
+    [SerializeField] private float currentHealth;
+    public float chipSpeed = 150f;
+    private float lerptimer;
+    public Image frontHealthBar;
+    public Image BackHealthBar;
     private void Start()
     {
         currentHealth = maxHealth;
     }
 
-    public void setHealth(int currentHealth)
+    private void Update()
     {
-        slider.value = currentHealth;
+        currentHealth = Mathf.Clamp(currentHealth,0,maxHealth);
+        UpdateHealthUI();
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            RestoreHealth(10f);
+
+        }
+    }
+    public void UpdateHealthUI()
+    {
+        Debug.Log(currentHealth);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = BackHealthBar.fillAmount;
+        float hFraction = currentHealth / maxHealth;
+
+        if (fillB > hFraction) 
+        {
+         frontHealthBar.fillAmount = hFraction;
+            BackHealthBar.color = Color.white;
+            lerptimer += Time.deltaTime;
+            float percentComplete = lerptimer / chipSpeed;
+            BackHealthBar.fillAmount = Mathf.Lerp(fillB,hFraction, percentComplete);
+
+
+        }
+        if (fillF < hFraction) 
+        {
+            BackHealthBar.color = Color.blue;
+            BackHealthBar.fillAmount = hFraction;
+            lerptimer += Time.deltaTime;
+            float percentComplete = lerptimer / chipSpeed;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, BackHealthBar.fillAmount, percentComplete);
+        }
+
+    }
+    public void setHealth(float currentHealth)
+    {
+        
     }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
+        lerptimer = 0f;
         setHealth(currentHealth);
 
         // Saðlýk deðerini kontrol et, örneðin karakter öldüyse baþka bir iþlem yapabilirsiniz
@@ -29,6 +69,12 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    public void RestoreHealth (float healAmount)
+    {
+     currentHealth += healAmount;
+        lerptimer = 0f;
+       
+    }
 
     private void Die()
     {
