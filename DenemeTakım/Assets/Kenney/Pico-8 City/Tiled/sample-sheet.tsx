7e274e448685fc44abf3d67583a2fcea71c08f81
retroento <emirayar@hotmail.com.tr>
2024-02-27 20:15:55 +0300

    Kenney Asset Pack

---
+++ DenemeTakım/Assets/Kenney/Pico-8 City/Tiled/sample-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="city-tileset" tilewidth="8" tileheight="8" spacing="1" tilecount="360" columns="24">
+ <image source="../Tilemap/tilemap.png" trans="ffaeb6" width="215" height="134"/>
+</tileset>
